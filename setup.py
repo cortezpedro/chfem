a149486a0ddf7a7c11c01439d842a7feaeb56377
Federico Semeraro <federico.semeraro@nasa.gov>
2024-03-04 13:25:42 -0800

    Cleaned setup.py and added find_package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 import os, glob, subprocess
 from pathlib import Path
@@ -24,36 +24,23 @@ class CustomBuildExt(build_ext):
     
     def build_extensions(self):
         built_objects = glob.glob(os.path.join(self.build_temp, "*.o"))
-        
         lib_filename = os.path.join(self.build_lib, self.ext_map['wrapper']._file_name)
         Path(os.path.split(lib_filename)[0]).mkdir(parents=True, exist_ok=True)
         linker_command = f"nvcc -Xcompiler -fopenmp -shared -o {lib_filename} {' '.join(built_objects)}"
         subprocess.check_call(linker_command, shell=True)
-        
-        exec_filename = os.path.join(os.path.split(os.path.join(self.build_lib, self.ext_map['wrapper']._file_name))[0], "chfem")
-        built_objects = [obj for obj in built_objects if 'wrapper' not in obj]
-        linker_command_exec = f"nvcc -Xcompiler -fopenmp -shared -o {exec_filename} {' '.join(built_objects)}"
-        subprocess.check_call(linker_command_exec, shell=True)
-        
         super().build_extensions()
 
 sources  = glob.glob('chfem/**/*.c', recursive=True)
 sources += glob.glob('chfem/**/*.cu', recursive=True)
 sources = [src for src in sources if '__pycache__' not in src]
 
-# Define the extension module
-chfem_module = Extension(
-    'chfem.wrapper',
-    sources=sources,
-    extra_link_args=['-fopenmp'],
-)
-
 setup(
     name='chfem',
     version='1.0',
-    description='Python interface for chfem simulation',
-    ext_modules=[chfem_module],
-    cmdclass={
-        'build_ext': CustomBuildExt,
-    }
+    author="chfem team",
+    url="https://gitlab.com/cortezpedro/chfem_gpu",
+    description='Python API for chfem',
+    packages=find_packages(),
+    ext_modules=[Extension('chfem.wrapper', sources=sources)],
+    cmdclass={'build_ext': CustomBuildExt}
 )
